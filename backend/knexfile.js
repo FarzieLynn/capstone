c2d41b3f849f58ed9c07088edbd608390ffcdd7a
Tanner <tanderson539@gmail.com>
2023-04-05 23:32:16 +0200

    More basic frameworking. Got a started server running with a knexfile and basic react app.

---
+++ backend/knexfile.js
@@ -0,0 +1,52 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user:process.env.DOCKER_USER,
+      password: process.env.DOCKER_PASSWORD,
+      port: process.env.DOCKER_PORT,
+      database: process.env.PG_DATABASE
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      user:process.env.DOCKER_USER,
+      password: process.env.DOCKER_PASSWORD,
+      port: process.env.DOCKER_PORT,
+      database: process.env.PG_DATABASE
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      user:process.env.DOCKER_USER,
+      password: process.env.DOCKER_PASSWORD,
+      port: process.env.DOCKER_PORT,
+      database: process.env.PG_DATABASE
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
