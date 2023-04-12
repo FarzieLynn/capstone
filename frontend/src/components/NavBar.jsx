6e35aa890ee636e211ae4db52453f79356276d95
FarzieLynn <farzanna.dube@outlook.com>
2023-04-12 13:38:24 -0700

    chat button in navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -48,6 +48,7 @@ const NavBar = () => {
             )}
           </Nav>
         </Nav>
+            <span><button className="btn" onClick={() => navigate('/chat')}>Click to Chat!</button></span>
         <Nav className="w-100 justify-content-evenly align-items-center">
           <span
             className="navbar-links fs-5"
