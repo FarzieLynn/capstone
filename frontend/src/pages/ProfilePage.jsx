1d5e5fe7d7ca5387944fdb9dbb8e75862c2cd6ff
AnthonyLaurio <anthonylaurio83@gmail.com>
2023-04-12 07:56:12 -0700

    Commit before merge

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -4,12 +4,16 @@ import { AppContext } from '../App'
 
 const ProfilePage = () => {
   const { user } = useContext(AppContext);
-
   console.log(user);
   return (
     <div className='profilepage-main'>
-      <h1>Profile Page</h1>
-      
+      <div className='profilepage-container'>
+        {user.publicData !== undefined ? <img className='profile-img' src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png" alt='Profile pic'/> : null}
+        {user.publicData !== undefined ? <h3>{user.publicData.full_name}</h3> : null}
+        {user.publicData !== undefined ? <h3>{user.publicData.branch}</h3> : null}
+        {user.publicData !== undefined ? <h3>{user.publicData.age_group}</h3> : null}
+        {user.publicData !== undefined ? <h3>{user.publicData.gender}</h3> : null}
+      </div>
     </div>
   )
 }
