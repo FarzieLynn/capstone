150979f04dd64d83826f378d5a3708ba90e48259
AnthonyLaurio <anthonylaurio83@gmail.com>
2023-04-17 09:34:11 -0700

    Added default test accounts, fixed navbar styling, and corrected some chat bugs

--- backend/seeds/01_users.js
+++ backend/seeds/01_users.js
@@ -30,7 +30,74 @@ exports.seed = async function (knex) {
     gender: "Male",
   };
 
-  for (let i = 2; i < usersToCreate; i++) {
+  const user1 = {
+    username: "user1",
+    password: bcrypt.hashSync("password", 12),
+    email: "user1@milanon.com",
+    branch: "USSF",
+    full_name: "User McUser",
+    age_group: "17-21",
+    gender: "Female",
+    is_professional: false,
+    is_verified: false,
+    is_anonymous: false,
+    anon_username: 'user12345678'
+  }
+  const user2 = {
+    username: "user2",
+    password: bcrypt.hashSync("password", 12),
+    email: "user2@milanon.com",
+    branch: "USSF",
+    full_name: "User McUser2",
+    age_group: "17-21",
+    gender: "Male",
+    is_professional: false,
+    is_verified: false,
+    is_anonymous: false,
+    anon_username: 'user87654321'
+  }
+  const chaplain = {
+    username: "chaplain",
+    password: bcrypt.hashSync("password", 12),
+    email: "chaplain@milanon.com",
+    branch: "USSF",
+    full_name: "Father Chaplain",
+    age_group: "17-21",
+    gender: "Male",
+    is_professional: true,
+    is_verified: false,
+    is_anonymous: false,
+    anon_username: 'user123'
+  }
+  const fitness = {
+    username: "fitness",
+    password: bcrypt.hashSync("password", 12),
+    email: "fitness@milanon.com",
+    branch: "USSF",
+    full_name: "Fit Woman",
+    age_group: "17-21",
+    gender: "Female",
+    is_professional: true,
+    is_verified: false,
+    is_anonymous: false,
+    anon_username: 'user1234'
+  }
+
+  const finance = {
+    username: "finance",
+    password: bcrypt.hashSync("password", 12),
+    email: "finance@milanon.com",
+    branch: "USSF",
+    full_name: "Finance Wizard",
+    age_group: "17-21",
+    gender: "Male",
+    is_professional: true,
+    is_verified: false,
+    is_anonymous: false,
+    anon_username: 'user12345'
+  }
+
+  for (let i = 7; i < usersToCreate; i++) {
     let user = {
       username: faker.internet.userName().toLowerCase(),
       password: bcrypt.hashSync("password", 12),
@@ -45,5 +112,10 @@ exports.seed = async function (knex) {
 
   await knex("users").insert(adminUser);
   await knex("users").insert(adminUser2);
+  await knex("users").insert(user1);
+  await knex("users").insert(user2);
+  await knex("users").insert(chaplain);
+  await knex("users").insert(fitness);
+  await knex("users").insert(finance);
   await knex("users").insert(fakeUserArray);
 };
--- backend/seeds/03_roles_users.js
+++ backend/seeds/03_roles_users.js
@@ -6,6 +6,9 @@ exports.seed = async function(knex) {
   
   await knex('role_users').insert([
     {user_id: 6, role_id:3},
-    {user_id:1, role_id:1}
+    {user_id:1, role_id:1},
+    {user_id:5, role_id:3},
+    {user_id:6, role_id:2},
+    {user_id:7, role_id:4},
   ]);
 };
