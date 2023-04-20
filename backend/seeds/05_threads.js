d2c54e74e9a510fccf74ac6ac0285127500571b1
AnthonyLaurio <anthonylaurio83@gmail.com>
2023-04-20 08:27:57 -0700

    More changes to faker

--- backend/seeds/05_threads.js
+++ backend/seeds/05_threads.js
@@ -3,12 +3,18 @@
  * @returns { Promise<void> } 
  */
 
-const { faker} = require("@faker-js/faker");
 
-exports.seed = async function(knex) {
-  await knex('threads').insert([
-    {thread_author: 3, thread_title:'Title!', thread_content: faker.lorem.paragraph(), thread_timestamp:knex.fn.now(), thread_type:'Mentorship'},
-    {thread_author: 6, thread_title:'Title!', thread_content: faker.lorem.paragraph(), thread_timestamp:knex.fn.now(), thread_type:'Mentorship'},
-    {thread_author: 4, thread_title:'Title!', thread_content: faker.lorem.paragraph(), thread_timestamp:knex.fn.now(), thread_type:'Mentorship'},
-  ]);
+
+exports.seed = async function (knex) {
+  if (process.env.NODE_ENV !== 'production') genData();
+
+  const genData = async () => {
+    const { faker } = require("@faker-js/faker");
+    await knex('threads').insert([
+      { thread_author: 3, thread_title: 'Title!', thread_content: faker.lorem.paragraph(), thread_timestamp: knex.fn.now(), thread_type: 'Mentorship' },
+      { thread_author: 6, thread_title: 'Title!', thread_content: faker.lorem.paragraph(), thread_timestamp: knex.fn.now(), thread_type: 'Mentorship' },
+      { thread_author: 4, thread_title: 'Title!', thread_content: faker.lorem.paragraph(), thread_timestamp: knex.fn.now(), thread_type: 'Mentorship' },
+    ]);
+  }
+
 };
--- backend/seeds/06_thread_comments.js
+++ backend/seeds/06_thread_comments.js
@@ -3,12 +3,18 @@
  * @returns { Promise<void> } 
  */
 
-const { faker} = require("@faker-js/faker");
 
-exports.seed = async function(knex) {
-  await knex('thread_comments').insert([
-    {thread_id:1, comment_author: 3, comment_content: faker.lorem.paragraph(), comment_timestamp:knex.fn.now()},
-    {thread_id:1, comment_author: 6, comment_content: faker.lorem.paragraph(), comment_timestamp:knex.fn.now()},
-    {thread_id:1, comment_author: 5, comment_content: faker.lorem.paragraph(), comment_timestamp:knex.fn.now()},
-  ]);
+
+exports.seed = async function (knex) {
+  if (process.env.NODE_ENV !== 'production') genData();
+
+  const genData = async () => {
+    const { faker } = require("@faker-js/faker");
+    await knex('thread_comments').insert([
+      { thread_id: 1, comment_author: 3, comment_content: faker.lorem.paragraph(), comment_timestamp: knex.fn.now() },
+      { thread_id: 1, comment_author: 6, comment_content: faker.lorem.paragraph(), comment_timestamp: knex.fn.now() },
+      { thread_id: 1, comment_author: 5, comment_content: faker.lorem.paragraph(), comment_timestamp: knex.fn.now() },
+    ]);
+  }
+
 };
